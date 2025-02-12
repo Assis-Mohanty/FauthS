0d0b2f6c7039ecaada85af28b5769f0208cf7a23
Assis-Mohanty <assis.mohanty.98@gmail.com>
2025-02-12 18:32:40 +0530

    added user_role  and roles

--- src/index.js
+++ src/index.js
@@ -4,15 +4,22 @@ const bodyParser=require("body-parser")
 const {PORT}=require("./config/serverConfig")
 const apiRoutes=require("./routes/index");
 const UserRepository=require("./repository/user-repository")
+const db=require("./models/index")
 const UserService=require("./services/user-service")
 const prepareAndStartServer=()=>{
-
+const {User,Role}=require("./models/index")
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}))
     app.use("/api",apiRoutes);
     app.listen(PORT,async()=>{
         console.log(`Server started on Port: ${PORT}`);
-    })
+        // if(process.env.DB_SYNC){
+        //     db.sequelize.sync({alter:true})
+        // }
+        const u1=await User.findByPk(4)
+        const r1 =await Role.findByPk(2)
+        u1.addRole(r1)
+    }) 
     const us=new UserService();
     // const newToken=us.createToken({email:"qqq@22sd.com",id:"ansdk"})
     // console.log("new token is ",newToken)
@@ -21,6 +28,8 @@ const prepareAndStartServer=()=>{
     // console.log(res)
     // const res=us.checkPassword("qqq","$2b$10$0UoabVn5HNJLiepDD.Q1rO6kAdJZgjkyxGYn33/A5lX04yKaVpQ92")
     // return res
+    
+    
 }
 
 prepareAndStartServer();
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,4 +6,12 @@ const router=express.Router();
 
 router.post("/signup",AuthRequestValidator.validateUserAuth,UserController.create)
 router.post("/signin",AuthRequestValidator.validateUserAuth,UserController.signIn)
+
+
+router.get("/isAuthenticated",UserController.isAuthenticated)
+
+router.get("/dummy",(req,res)=>{
+    return res.status(200).json({message:"OK"})
+})
+
 module.exports=router;
