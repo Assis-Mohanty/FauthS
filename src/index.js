bb5339aad43bfec66ff194b5402aa79511094660
Assis-Mohanty <assis.mohanty.98@gmail.com>
2025-02-12 11:23:13 +0530

    added get userById

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ const app=express();
 const bodyParser=require("body-parser")
 const {PORT}=require("./config/serverConfig")
 const apiRoutes=require("./routes/index");
-
+const UserRepository=require("./repository/user-repository")
 const prepareAndStartServer=()=>{
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}))
     app.use("/api",apiRoutes);
-    app.listen(PORT,()=>[
-        console.log(`Server started `)
-    ])
+    app.listen(PORT,async()=>{
+        console.log(`Server started on Port: ${PORT}`);
+    })
+    
 }
 
 prepareAndStartServer();
